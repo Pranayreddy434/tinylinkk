088e2366467b470122e99b21b0c18b5dd229f804
Pranayreddy434 <133482226+Pranayreddy434@users.noreply.github.com>
2025-11-22 17:03:29 +0530

    Fixed .env file

--- server/db.js
+++ server/db.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '20g21a0434_P',
+  database: process.env.DB_NAME || 'tinylink',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
